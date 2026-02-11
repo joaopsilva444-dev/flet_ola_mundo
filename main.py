cf9f2cc7c02773784364aaf99ffeb737f06be59b
João Pedro Miguel <joao.p.silva444@aluno.senai.br>
2026-02-11 14:20:04 -0300

    meu primeiro app flet

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import flet as ft
 
 def main(page: ft.Page):
+    page.title = "Meu Primeiro app Flet"
+    page.bgcolor = "#47c3fc"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.add(
         ft.Text(value="Mambo"),
         ft.ElevatedButton("Clique Aqui")
